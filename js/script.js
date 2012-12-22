a42b51a1173b90eee02aec07e228a318914bb9c0
Annabelle Buffart <anna.buffart@gmail.com>
2012-12-22 13:45:19 +0100

    Test jQueryMobile

--- js/script.js
+++ js/script.js
@@ -5,24 +5,31 @@
 
     $(document).on("mobileinit", function(){
 
-        var spanH3 = $(".ui-btn-text");
+       var spanH3 = $("ui-btn-text");
 
         $.ajax({
             url : sUrl,
             dataType : "jsonp",
             success : function(data){
+                console.log(spanH3);
                 console.log(data.root.show.title);
-                console.log(spanH3.text(data.root.show.title));
+                spanH3.text(data.root.show.title);
+
+                var output = '<h3>test</h3>';
+                //now append the buffered output to the listview and either refresh the listview or create it (meaning have jQuery Mobile style the list)
+                //or if the listview has yet to be initialized, use `.trigger('create');` instead of `.listview('refresh');`
+                $('.serieTitle').append(output).trigger('create');
 
             }
+
         });
         //JSONP uniquement pr qd on est en local
+
+
     });
-
-
 
     $(function(){
 
-	});
+    });
 
 })(jQuery);
