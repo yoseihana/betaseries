712700292fac03ab61c235092b33039d6c167621
Annabelle Buffart <anna.buffart@gmail.com>
2013-01-05 16:48:47 +0100

    4 fonctionnalités ok
    TODO: - CSS
    - Pls sessionsStorage
    - sessionStorage sur épisode

--- js/script.js
+++ js/script.js
@@ -3,45 +3,42 @@
     //Définition des variable
     var $oError, $oInputVal, $oSearchResult;
 
+    //Test le champ de recherche
+    var searchTest = function (e) {
 
-    //Ajout de la série dans le sessionsStorage
-    var addSerie = function (serie) {
-        console.log("test add ok");
-        var monobjet = serie;
-        var monobjet_json = JSON.stringify(monobjet);
-        localStorage.setItem("objet", monobjet_json);
-        showMySeries();
-    }
+        $oSearchResult = $('.searchResult');
 
-    //Affichage du sessionStorage
-    var showMySeries = function () {
-        var monobjet_json = localStorage.getItem("objet");
-        var monobjet = JSON.parse(monobjet_json);
+        if (e.which == 13) {
 
-        $(".favoritSeries").append("<li>" + monobjet.description + "</li>");
-        console.log(monobjet.description);
-    }
+            if ($('input').val() !== "") {
 
-//Affiche le listing de mes séries
-    /*var showMesSeries = function (shows) {
+                if ($('input').val().length < 2) {
+                    $oSearchResult.find('p').remove();
+                    $oSearchResult.find('li').remove();
+                    $oSearchResult.find('div').append('<p>Il faut minimum 2 lettres pour avoir un résultat de recherche</p>');
+                }
+                else {
+                    $oSearchResult.find('p').remove();
+                    $oSearchResult.find('li').remove();
 
-     /*  MES SERIES */
-    /* var listingMesSeries = $(".pageFavorit ul");
+                    searchSerie();
+                }
 
-     for (y in shows) {
-     listingMesSeries.append('<h3>' + shows.title + '</h3>');
-     listingMesSeries.append('<div class="moreInfo"></div>');
-     listingMesSeries.append("<button>Enlever de mes favoris</button>");
-     }
-     // $("ul").listview();
-     };*/
-
+            }
+            else {
+                $oSearchResult.find('p').remove();
+                $oSearchResult.find('li').remove();
+                $oSearchResult.find('div').append('<p>Entrez le nom d\'une série pour la retrouvée dans le listing de BetaSerie</p>');
+            }
+        }
+    };
 
     //Recherche d'une série
     var searchSerie = function (e) {
         var sMySerie = $("#searchSerie").val();
         var sSearch = "http://api.betaseries.com/shows/search.json?title=" + sMySerie + "&key=bf7f369ce177";
         var $searchList = $(".searchList");
+        $oSearchResult = $('.searchResult');
 
         $.ajax({
             url:sSearch,
@@ -50,11 +47,20 @@
                 var rechercheSection = $searchList
                 var searchResult = dataSearch.root.shows;
 
-                for (b in searchResult) {
-                    rechercheSection.append('<li ><a href="javascript:void(0);" rel="serieDescription" class="' + searchResult[b].url + '">' + searchResult[b].title + '</a></li>');
-                }
+                if (searchResult.length < 1) {
+                    $oSearchResult.find('p').remove();
+                    $oSearchResult.find('li').remove();
+                    $oSearchResult.find('div').append('<p>Il n\'y a pas de résultat pour cette recherche.</p>');
 
-                $searchList.find('a').on('click', listSerieClick)
+                } else {
+
+                    for (b in searchResult) {
+                        rechercheSection.append('<li><a href="javascript:void(0);" rel="serieDescription" class="' + searchResult[b].url + '">' + searchResult[b].title + '</a></li>');
+                        urlResult = searchResult[b].url;
+                    }
+
+                    $searchList.find('a').on('click', listSerieClick)
+                }
             }
 
         });
@@ -62,8 +68,16 @@
 
     //Déscription de la série après click dans le listing de recherche
     var listSerieClick = function () {
-        var tabId = $(".searchList a").attr('rel');
-        var sUrlSerie = $(".searchList a").attr('class');
+
+        if ($('.searchList a').length) {
+
+            var tabId = $(".searchList a").attr('rel');
+            var sUrlSerie = $(".searchList a").attr('class');
+
+        } else if ($('.favoritList a').length) {
+            var tabId = $(".favoritList a").attr('rel');
+            var sUrlSerie = $(".favoritList a").attr('class');
+        }
 
         history.pushState({
             id:tabId
@@ -84,7 +98,7 @@
         //  $(document).on("mobileinit", function () {
         var sUrl = "http://api.betaseries.com/shows/display/" + sUrlSerie + ".json?key=bf7f369ce177";
 
-        $("#serieDescription p").remove();
+        $("#serieDescription .error").remove();
 
         $(document).load($.ajax({
             url:sUrl,
@@ -99,22 +113,48 @@
                 $(".timing").append(serie.duration);
                 $(".status").append(serie.status);
 
-
                 //Boucle des seasons
                 for (x in serie.seasons) {
                     var seasons = serie.seasons[x].number;
                     var episodes = serie.seasons[x].episodes;
 
                     $(".seasons").append("<li><p> Saisons: " + seasons + "</p><p>Nombre d'épisodes: " + episodes + "</p></li>");
-
                 }
 
                 $(".addFavorit").on("click", function () {
-                    addSerie(serie)
+                    addSerie(serie);
 
                 });
             }
         }))
+    };
+
+    //Ajout de la série dans le sessionsStorage
+    var addSerie = function (serie) {
+        var oSerieFav = serie;
+        var oSerieFav_json = JSON.stringify(oSerieFav);
+
+        localStorage.setItem('objet', oSerieFav_json)
+
+        showMySeries();
+    }
+
+    //Affichage du sessionStorage
+    var showMySeries = function () {
+        var oSerieFav_json = localStorage.getItem('objet');
+        var oSerieFav = JSON.parse(oSerieFav_json);
+
+        $('.favoritList').append('<li></li>');
+        $('.favoritList li').append('<h3><a href="javascript:void(0);" rel="serieDescription" class="' + oSerieFav.url + '">' + oSerieFav.title + '</a></h3>');
+        $('.favoritList li').append('<p>Etat de la série: ' + oSerieFav.status + '</p>');
+        var len = $.map(oSerieFav.seasons,function (n, i) {
+            return i;
+        }).length;
+
+        $('.favoritList li').append('<p>Nombre de saisons: ' + len + '</p>');
+
+        $('h3').on('click', listSerieClick);
+
     };
 
 //JSONP uniquement pr qd on est en local
@@ -149,42 +189,50 @@
         $("div#" + tabId).addClass('active');
 
         if (tabId == 'serieDescription') {
-            $oSerieDescription.find("section").remove();
-            $oSerieDescription.append("<p>Il n'y a pas de série sélectionnée. Faites d'abord une recherche.</p>");
+            $("#serieDescription section").remove();
+            $("#serieDescription .error").remove()
+            $oSerieDescription.append('<p class="error">Il n\'y a pas de série sélectionnée. Faites d\'abord une recherche.</p>');
+        }
+
+        if (tabId == "myFavorit") {
+            $('.favoritList li').remove();
+            showMySeries();
+        }
+
+        if (tabId == "planning") {
+            $('.planningList li').remove();
+            showPlanning();
         }
     };
 
-//Load de routine
-    $(function () {
-        $oError = $(".searchError p");
-        $oInputVal = $("input").val();
-        $oSearchResult = $(".searchResult li");
+    var showPlanning = function () {
+        var sPlanningApi = "http://api.betaseries.com/planning/incoming.json?key=bf7f369ce177";
+        var oSerieFav_json = localStorage.getItem('objet');
+        var oSerieFav = JSON.parse(oSerieFav_json);
 
-        $(this).keypress(function (e) {
-            if (e.which == 13) {
-                if ($("input").val() !== "") {
-                    if ($("input").val().length < 2) {
-                        $oError.remove();
-                        $oSearchResult.remove();
-                        $oError.append('<p>Il faut minimum 2 lettres pour avoir un résultat de recherche</p>');
-
+        $(document).load($.ajax({
+            url:sPlanningApi,
+            dataType:'jsonp',
+            success:function (dataPlanning) {
+                var oPlanning = dataPlanning.root.planning;
+                for (c in oPlanning) {
+                    if (oPlanning[c].show == oSerieFav.title) {
+                        $('.planningList').append('<li></li>');
+                        $('.planningList li').append('<p>' + oPlanning[c].show + '</p>');
+                        $('.planningList li').append('<p>' + oPlanning[c].number + '</p>');
+                        $('.planningList li').append('<p>' + new Date(oPlanning[c].date) + '</p>');
+                    } else if (oSerieFav.status == 'Ended') {
+                        console.log("fini");
                     }
-                    else {
-                        $oError.remove();
-                        $oSearchResult.remove();
-
-                        searchSerie();
-                    }
-
-                }
-                else {
-                    $oError.remove();
-                    $oSearchResult.remove();
-                    $oError.append("<p>Entrez le nom d'une série pour la retrouvée dans le listing de BetaSerie</p>")
                 }
             }
-        });
+        }));
+    }
 
+//Load de routine
+    $(function () {
+
+        $(this).on('keypress', searchTest);
         $('ul.nav-tabs li a').on('click', menuClick);
 
     });
